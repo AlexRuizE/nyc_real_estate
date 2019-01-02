3a992de0aa16596753d86dab02b0f6fbd781b08e
Alex RE <monorhesus@localhost.localdomain>
2019-01-01 19:11:12 -0500

    One year data

--- nyc_sales_scrape.py
+++ nyc_sales_scrape.py
@@ -1,5 +1,43 @@
-"""Script to update monthly NYC sales data."""
-
+"""Script to update monthly NYC sales data (nyc.gov)."""
 
 import requests
+import bs4
+import pandas as pd
+
+# Variables
+homeUrl = "https://www1.nyc.gov"
+salesUrl = "/site/finance/taxes/property-rolling-sales-data.page"
+dataDir = "/home/monorhesus/Dropbox/codigo/data/nyc_rolling_sales/"
+
+
+# Get borough sales urls
+mainPage = requests.get(homeUrl+salesUrl)
+assert mainPage.ok, "Could not get main sales page. "
+html = bs4.BeautifulSoup(mainPage.text, "lxml")
+htmlTable = html.find("table")
+links = [link.get("href") for link in htmlTable.find_all("a")]
+links = [link for link in links if ".xls" in link]
+dataLinks = [homeUrl+link for link in links]
+
+# Get data
+fileNames=[]
+for link in dataLinks:
+	fileName = link.split("/")[-1]
+	fileNames.append(fileName)
+	getData = requests.get(link)
+	if getData.ok:
+		with open(dataDir+fileName, "wb") as f:
+			f.write(getData.content)
+			print("Saved {} in {}".format(link, dataDir))
+	else:
+		print("Link {} not retreived.".format(link))
+
+
+# Update dataframe
+skipRows=4
+df = pd.DataFrame()
+for file in fileNames:
+	dfTemp = pd.read_excel(dataDir+fileName, skiprows=skipRows)
+	df = df.append(dfTemp)
+df.colums = 
 
