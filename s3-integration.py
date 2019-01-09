6f9c98dad6d09c817226dcd1ab5c594de6d0229d
Alex RE <monorhesus@localhost.localdomain>
2019-01-08 22:12:09 -0500

    backblaze stuff

--- s3-integration.py
+++ s3-integration.py
@@ -1,5 +1,7 @@
 """Integrate S3 data to workflow"""
 
+import os
+from b2blaze import B2
 import boto3
 
 def getS3Data(bucket=None, key=None):
@@ -14,4 +16,27 @@ def getS3Data(bucket=None, key=None):
 
 def putS3Data():
 	pass
+
+
+
+
+class IODataTrendcast:
+	"""Using B2"""
+	key_id="002b76795509bd80000000002"
+	application_key="K0022KfEz54NeI7MvorXXivHq0vfWqY"
+
+	def __init__(self, bucketName):
+		self.b2 = B2(key_id=key_id, application_key=application_key)
+		self.bucket = b2.buckets.get(bucketName) 
+
+	def putLocal(self, localFile, remoteFile, numThreads):
+		"""Put local file in remote bucket"""
+
+		fileSize = os.path.getsize(localFile)/1000000 # Mb
+		if fileSize>500:
+			with open(localFile, "rb") as f:
+				bucket.files.upload_large_file(contents=f, file_name=remoteFile, num_threads=numThreads)
+		else:
+
+
 
