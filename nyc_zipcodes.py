8d7a09b05dd64da4b18adbe8299a3b2ec4a8add1
Alex Ruiz E <ruiz.euler@gmail.com>
2019-02-02 16:02:07 -0500

    token a

--- nyc_zipcodes.py
+++ nyc_zipcodes.py
@@ -3,7 +3,7 @@
 import geopandas as gpd
 %matplotlib
 
-nycZips = "nyc_zipcodes/ZIP_CODE_040114.shp" 
+nycZips = "nyc_zipcodes/ZIP_CODE_040114.shp"  
 z = gpd.read_file(nycZips)
 
 z.plot(color='white', edgecolor='darkgray')
